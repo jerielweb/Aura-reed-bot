ba41cde4507ae51b1f8f2640bca4f70f3d7a516e
Jeriel Beckford <jerielweb@hotmail.es>
2026-06-02 17:41:33 -0600

    feat: mejorar mensajes de error para la promoción de usuarios en grupos

--- commands/group/promote.js
+++ commands/group/promote.js
@@ -22,14 +22,24 @@ export default {
 
         let userToPromote = message.message?.extendedTextMessage?.contextInfo?.participant || message.message?.extendedTextMessage?.contextInfo?.mentionedJid?.[0];
         if (!userToPromote) {
-            return socket.sendMessage(remoteJid, { text: warningMessage('FALTA USUARIO', 'Etiqueta o responde al mensaje de alguien para darle admin.') }, { quoted: message });
+
+            // Plantilla del mensaje para que sea más atractivo visualmente
+            let text = `╭〔 ⚠️ ${fytBold('AURA REED')} 〕⬣\n`;
+            text += `┃ ${fytBold('FALTA USUARIO')} \n`;
+            text += `╰━━━━━━━━━━━━⬣\n\n`;
+            text += `┃ > Etiqueta o responde al mensaje de alguien\n`;
+            text += `┃ > para darle admin.\n\n`;
+            text += `╰〔 ⚡ ${fytBold('SYSTEM INFO')} 〕⬣`;
+
+            // Enviar mensaje de error
+            return socket.sendMessage(remoteJid, { text }, { quoted: message });
         }
 
         try {
             await socket.groupParticipantsUpdate(remoteJid, [userToPromote], "promote");
 
             // Mensaje de éxito Plantilla del mensaje para que sea más atractivo visualmente
-            let text = `╭〔  ${fytBold('ADMIN SYSTEM')} 〕⬣\n`
+            let text = `╭〔  ${fytBold('ADMIN SYSTEM')} 〕⬣\n\n`
             text += `┃ > El usuario @${userToPromote.split('@')[0]}\n`;
             text += `┃ > ahora es administrador del grupo\n\n`;
             text += `╰〔 ⚡ ${fytBold('AURA REED')} 〕⬣`;
