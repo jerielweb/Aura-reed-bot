d549d5864ef535cadf3066d0776d5c9314b328ba
Jeriel Beckford <jerielweb@hotmail.es>
2026-06-02 18:22:55 -0600

    feat: mejorar mensajes de restricción para mayor claridad y formato visual

--- controllers/textBots.js
+++ controllers/textBots.js
@@ -1,8 +1,11 @@
 //Restricciones
+import { fytBold } from './../models/TextStyle.js';
+
+
 export const Rstr = {
-    onlyAdmin: '╭〔 🛡️ 𝐀𝐔𝐑𝐀 𝐑𝐄𝐄𝐃 〕⬣\n┃ ⚠️ 𝐀𝐂𝐂𝐄𝐒𝐎 𝐃𝐄𝐍𝐄𝐆𝐀𝐃𝐎\n╰━━━━━━━━━━━━⬣\n\n┃ > Este comando es exclusivo\n┃ > para administradores del grupo.\n\n╰〔 ⚡ 𝐒𝐘𝐒𝐓𝐄𝐌 〕⬣',
-    onlyOwner: '╭〔 👑 𝐀𝐔𝐑𝐀 𝐑𝐄𝐄𝐃 〕⬣\n┃ ⚠️ 𝐀𝐂𝐂𝐄𝐒𝐎 𝐃𝐄𝐍𝐄𝐆𝐀𝐃𝐎\n╰━━━━━━━━━━━━⬣\n\n┃ > Este comando es exclusivo\n┃ > para el propietario del bot.\n\n╰〔 ⚡ 𝐒𝐘𝐒𝐓𝐄𝐌 〕⬣',
-    onlyGroup: '╭〔 ⚠️ 𝐀𝐃𝐌𝐈𝐍 𝐒𝐘𝐒𝐓𝐄𝐌 〕⬣\n\n┃ ❌ 𝐀𝐂𝐂𝐈𝐎́𝐍 𝐃𝐄𝐍𝐄𝐆𝐀𝐃𝐀\n┃ > solo funciona en grupos\n\n╰〔 ⚡ 𝐀𝐔𝐑𝐀 𝐑𝐄𝐄𝐃 〕⬣',
+    onlyAdmin: `╭〔 ⚠️ ${fytBold('AURA REED')} 〕⬣\n┃ ${fytBold('ACSESO DENEGADO')} \n╰━━━━━━━━━━━━⬣\n\n┃ > Este comando es exclusivo\n┃ > para administradores del grupo.\n\n╰〔 ⚡ ${fytBold('SYSTEM ALERT')} 〕⬣`,
+    onlyOwner: `╭〔 ⚠️ ${fytBold('AURA REED')} 〕⬣\n┃ ${fytBold('ACSESO DENEGADO')} \n╰━━━━━━━━━━━━⬣\n\n┃ > Este comando es exclusivo\n┃ > para el propietario del bot.\n\n╰〔 ⚡ ${fytBold('SYSTEM ALERT')} 〕⬣`,
+    onlyGroup: `╭〔 ❌ ${fytBold('AURA REED')} 〕⬣\n┃ ${fytBold('ACCION INCONPATIBLE')} \n╰━━━━━━━━━━━━⬣\n\n┃ > Este comando solo funciona en grupos.\n\n╰〔 ⚡ ${fytBold('SYSTEM ALERT')} 〕⬣`,
     onlyPrivate: 'Este comando solo se puede usar en privado'
 }
 
