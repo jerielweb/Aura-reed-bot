6516baddd97db5afa6d71c85da65bd8333bd4bed
Jeriel <Yo>
2026-08-03 22:03:23 -0600

    update ahorcado.js

--- commands/games/ahorcado.js
+++ commands/games/ahorcado.js
@@ -98,7 +98,7 @@ async function generateHangmanImage(game) {
     .map((letter) => (game.guessedLetters.has(letter) ? letter.toUpperCase() : "_"))
     .join(" ");
 
-  ctx.font = "regular 90px Arial";
+  ctx.font = "90px Arial";
   ctx.fillStyle = "#000000";
   ctx.textAlign = "center";
   ctx.fillText(displayWord, 400, 750);
