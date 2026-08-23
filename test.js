c3c2be771a89d0efcdee43e23d3671f71dc909e6
jerielweb <jerielweb@outlook.es>
2026-08-23 10:46:24 -0600

    update test.js

--- test.js
+++ test.js
@@ -1,16 +1,17 @@
-import yt from "@vreden/youtube_scraper";
+import TikTok from "@tobyg74/tiktok-api-dl";
 
 async function testVideo() {
   try {
-    const url = "https://youtu.be/QqJ9zrY_ITw"; // Puedes cambiar el enlace aquí
+    const url = "https://vt.tiktok.com/ZSVHkGC1n/"; // Puedes cambiar el enlace aquí
     console.log("Obteniendo video...");
     
     // La librería usa yt.ytmp4 para videos
-    const res = await yt.ytmp4(url);
-    
-    console.log("Resultado completo:", JSON.stringify(res, null, 2));
-  } catch (error) {
-    console.error("Error:", error);
+    Tiktok.Downloader(url, {
+      version: "v1",
+      showOriginalResponse: true
+    }).then((result) => console.log(result))
+  } catch(error) {
+    console.error(error)
   }
 }
 
