52e0e9cebcc8500b3a62e6397d9cf1a8e7d1d506
jerielweb <jerielweb@outlook.es>
2026-08-23 16:37:32 -0600

    update tiktok.js

--- commands/downloads/tiktok.js
+++ commands/downloads/tiktok.js
@@ -84,7 +84,7 @@ async function processVideoFile(inputP, outP) {
 
   const targetSizeBits = MAX_SIZE_MB * 8 * 1024 * 1024 * 0.85;
   const audioBitrate = 96;
-  const videoBitrate = Math.max(310, Math.floor(targetSizeBits / duration / 1000) - audioBitrate);
+  const videoBitrate = Math.max(320, Math.floor(targetSizeBits / duration / 1000) - audioBitrate);
 
   await execAsync(
     `ffmpeg -i "${inputP}" -vf "scale='min(1920,iw)':-2" -threads 2 -c:v libx264 -preset veryfast ` +
